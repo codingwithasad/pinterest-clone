33dd2801835454e361a64d55d0c202c3a6d44877
Asad Mughal <153642328+codingwithasad@users.noreply.github.com>
2024-01-18 18:20:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 const expressSession = require("express-session");
 const flash = require('connect-flash');
-const port = 3000;
+const port =process.env.PORT || 3000;
 
 
 var express = require('express');
